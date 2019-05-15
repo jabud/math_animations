b737f62c75b3a8a183e68bc86f25202a76165fe1
jfreek <jorgee.abud@gmail.com>
2019-05-15 13:51:26 -0600

    hyperbolic kernel example

--- svm/hyperkernel.py
+++ svm/hyperkernel.py
@@ -5,6 +5,7 @@ import math
 import sklearn
 from matplotlib import font_manager as fm, rcParams
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 from matplotlib import cm
 from mpl_toolkits import mplot3d
 from mpl_toolkits.mplot3d import Axes3D
@@ -15,25 +16,12 @@ class HypKernel:
     # config canavas
     def __init__(self, theme):
         # colors for dark bg
-        if theme=='dark':
-            self.bg_color = '#232323'
-            self.fig_color = '#232323'#d8d8d8
-            self.grid_color = '#7c2230'
-            self.data_color = '#20ad9d'
-            self.funct_color = '#e8dc55'
-        # colors for transparent versions
-        elif theme=='light':
-            self.bg_color = '#e2deb3'
-            self.fig_color = '#232323'
-            self.grid_color = '#46abea'
-            self.data_color = '#3c8c48'
-            self.funct_color = '#c43838'
+        self.bg_color = '#232323'
         self.fig = plt.figure(facecolor=self.bg_color)
         self.ax = Axes3D(self.fig, facecolor=self.bg_color)
+        self.cid = self.fig.canvas.mpl_connect('key_press_event', self.key_press)
         fpath = os.path.join(rcParams["datapath"], "alterebro_pixel_font/alterebro-pixel-font.ttf")
         self.prop = fm.FontProperties(fname=fpath)
-        # plt.xticks(ticks=[], labels='')
-        # plt.yticks(ticks=[], labels='')
         self.ax.spines['left'].set_color('none')
         self.ax.spines['bottom'].set_color('none')
 
@@ -50,11 +38,6 @@ class HypKernel:
         y_large_purple = np.random.normal(loc=loc, scale=scale, size=n_points) 
         x_small_purple = np.random.normal(loc=-loc, scale=scale, size=n_points) 
         y_small_purple = np.random.normal(loc=-loc, scale=scale, size=n_points) 
-        # Plot example 1 on a hyperbolic surface.
-        x = np.arange(-2, 2, 0.05)
-        y = np.arange(-2, 2, 0.05)
-        x, y = np.meshgrid(x, y)
-        hyp = x * y
         zmin = -2.
         zmax = 2.
 
@@ -79,26 +62,70 @@ class HypKernel:
 
         self.ax.view_init(elev=90,azim=90)
 
-        # animate
-        # self.ax.plot_surface(
-        #     x, y, hyp,
-        #     cmap=cm.summer,
-        #     alpha=.3,
-        #     linewidth=0,
-        #     antialiased=False,
-        # )
-        # self.ax.contour(
-        #     x, y, hyp,
-        #     cmap=cm.summer,
-        # )
+    def hyper_anim(self, i):
+        x = np.arange(-2, 2, 0.05)
+        y = np.arange(-2, 2, 0.05)
+        x, y = np.meshgrid(x, y)
+        hyp = x * y
+        self.ax.plot_surface(
+            x, y, hyp,
+            cmap=cm.summer,
+            alpha=.01*i,
+            linewidth=0,
+            antialiased=False,
+        )
+        return self.ax,plt
+
+    def animate_hyper(self, f):
+        anim = animation.FuncAnimation(self.fig, self.hyper_anim, interval=40, 
+                                        frames=f, blit=False, repeat=False)
+
+        plt.show()
+    
+    def plane_anim(self, i):
         # Plot a good planar discriminator.
-        # plane = 0. * x
-        # self.ax.plot_surface(
-        #     x, y, plane,
-        #     alpha=.3,
-        #     linewidth=0,
-        #     antialiased=False,
-        # )
+        x = np.arange(-2, 2, 0.05)
+        y = np.arange(-2, 2, 0.05)
+        x, y = np.meshgrid(x, y)
+        plane = 0. * x
+        self.ax.plot_surface(
+            x, y, plane,
+            alpha=.02*(i),
+            linewidth=0,
+            antialiased=False,
+        )
+        return self.ax,plt
+
+    def animate_plane(self, f):
+        anim = animation.FuncAnimation(self.fig, self.plane_anim, interval=40, 
+                                        frames=f, blit=False, repeat=False)
+
+        plt.show()
+
+    def key_press(self, event):
+        # Save Image with NO background color
+        if event.key == 'I':
+            plt.savefig('img.png', dpi=None, transparent=True)
+        # Save Image with background color
+        elif event.key == 'i':
+            plt.savefig('img_bg.png', dpi=None, facecolor='w')
+        # Reset
+        elif event.key == '0':
+            # some action to reset
+            plt.cla()
+            plt.xticks(ticks=[], labels='')
+            plt.yticks(ticks=[], labels='')
+            self.ax.spines['left'].set_color('none')
+            self.ax.spines['bottom'].set_color('none')
+        # Separeted animations    
+        elif event.key=='1':
+            # some action
+            self.animate_hyper(10)
+        elif event.key=='2':
+            # some action
+            self.animate_plane(10)
+
+        plt.show()
 
 
 def main():
@@ -106,16 +133,13 @@ def main():
     rcParams['toolbar'] = 'None'
 
     # Initiate class
-    svm = HypKernel(theme='dark')
+    svm = HypKernel()
     
+    # example
     svm.cornersexample()
 
     # Configure position of graph in canvas
-    # Centered
     plt.subplots_adjust(left=.5, bottom=.2, right=.9, top=.8, wspace=.20, hspace=.20)
-    
-    # Full page
-    # plt.subplots_adjust(left=0, bottom=0, right=1, top=1, wspace=.20, hspace=.20)
 
     # showtime
     plt.show()
