ce3f0d3441c453ee09c4546c1d843400ee9e7eed
jfreek <jorgee.abud@gmail.com>
2019-05-15 13:51:37 -0600

    kernel surfaces examples

--- svm/kernel_tricks.py
+++ svm/kernel_tricks.py
@@ -1,262 +1,128 @@
-
-import numpy as np
+import os
+import numpy as np 
+import sklearn
+from matplotlib import font_manager as fm, rcParams
 from matplotlib import cm
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 
-# Plot example 1 on a hyperbolic surface.
-x = np.arange(-2, 2, 0.05)
-y = np.arange(-2, 2, 0.05)
-x, y = np.meshgrid(x, y)
+class KernelExamples:
+    # config canavas
+    def __init__(self, theme):
+        # colors for dark bg
+        self.bg_color = '#232323'
+        self.fig = plt.figure(facecolor=self.bg_color)
+        self.ax = Axes3D(self.fig, facecolor=self.bg_color)
+        fpath = os.path.join(rcParams["datapath"], "alterebro_pixel_font/alterebro-pixel-font.ttf")
+        self.prop = fm.FontProperties(fname=fpath)
+        self.ax.spines['left'].set_color('none')
+        self.ax.spines['bottom'].set_color('none')
+        x = np.arange(-2, 2, 0.05)
+        y = np.arange(-2, 2, 0.05)
+        self.x, self.y = np.meshgrid(x, y)
 
-# Plot other surfaces that can be used for transformations.
 
-# A quadratic surface.
-fig = plt.figure(4)
-ax = fig.add_subplot(111, projection='3d')
+    def quadratic_surface(self):
+        quadratic = self.x**2 * self.y**2
+        self.ax.plot_surface(
+            self.x, self.y, quadratic,
+            cmap=cm.summer,
+            alpha=.3,
+            linewidth=0,
+            antialiased=False,
+        )
+        self.ax.contour(self.x, self.y, quadratic, cmap=cm.summer)
+        self.ax.set_zticklabels([])
+        self.ax.set_yticklabels([])
+        self.ax.set_xticklabels([])
+        self.ax.axis([-2., 2., -2., 2.])
+        self.ax.set_zlim([0, 4.])
 
-quadratic = x**2 * y**2
-ax.plot_surface(
-    x, y, quadratic,
-    cmap=cm.summer,
-    alpha=.3,
-    linewidth=0,
-    antialiased=False,
-)
-ax.contour(x, y, quadratic, cmap=cm.summer)
-ax.set_zticklabels([])
-ax.set_yticklabels([])
-ax.set_xticklabels([])
-ax.axis([-2., 2., -2., 2.])
-ax.set_zlim([0, 4.])
+    def rippled_surface(self):
+        # A rippled surface.
+        ripple = np.cos((self.x**2 + self.y**2)**.5)**2
+        self.ax.plot_surface(
+            self.x, self.y, ripple,
+            cmap=cm.summer,
+            alpha=.3,
+            linewidth=0,
+            antialiased=False,
+        )
+        self.ax.contour(self.x, self.y, ripple, cmap=cm.summer)
+        self.ax.set_zticklabels([])
+        self.ax.set_yticklabels([])
+        self.ax.set_xticklabels([])
+        self.ax.axis([-2., 2., -2., 2.])
+        self.ax.set_zlim([0., 1.])
 
-fig = plt.figure(5)
-ax = fig.add_subplot(111, projection='3d')
+    def peaked_surface(self):
+        peaked = np.cos(2 * self.x) * np.sin(3 * self.y)
+        self.ax.plot_surface(
+            self.x, self.y, peaked,
+            cmap=cm.summer,
+            alpha=.3,
+            linewidth=0,
+            antialiased=False,
+        )
+        self.ax.contour(self.x, self.y, peaked, cmap=cm.summer)
+        self.ax.set_zticklabels([])
+        self.ax.set_yticklabels([])
+        self.ax.set_xticklabels([])
+        self.ax.axis([-2., 2., -2., 2.])
+        self.ax.set_zlim([-1., 1.])
 
-# A rippled surface.
-ripple = np.cos((x**2 + y**2)**.5)**2
-ax.plot_surface(
-    x, y, ripple,
-    cmap=cm.summer,
-    alpha=.3,
-    linewidth=0,
-    antialiased=False,
-)
-ax.contour(x, y, ripple, cmap=cm.summer)
-ax.set_zticklabels([])
-ax.set_yticklabels([])
-ax.set_xticklabels([])
-ax.axis([-2., 2., -2., 2.])
-ax.set_zlim([0., 1.])
+    def quadratic_surface2(self):
+        # Optionally, show the resulting quadratic surface.
+        show_surface = True
+        shift = 2.2
+        if show_surface:
+            dual_quad = self.y**2 - shift
+            self.ax.plot_surface(
+                self.x, self.y, dual_quad,
+                cmap=cm.summer,
+                alpha=.3,
+                linewidth=0,
+                antialiased=False,
+            )
 
-# A peaked surface.
-fig = plt.figure(6)
-ax = fig.add_subplot(111, projection='3d')
-peaked = np.cos(2 * x) * np.sin(3 * y)
-ax.plot_surface(
-    x, y, peaked,
-    cmap=cm.summer,
-    alpha=.3,
-    linewidth=0,
-    antialiased=False,
-)
-ax.contour(x, y, peaked, cmap=cm.summer)
-ax.set_zticklabels([])
-ax.set_yticklabels([])
-ax.set_xticklabels([])
-ax.axis([-2., 2., -2., 2.])
-ax.set_zlim([-1., 1.])
+        # Optionally, show a good planar discriminator.
+        show_discriminator = False
+        if show_discriminator:
+            plane = 0. * self.x
+            self.ax.plot_surface(
+                self.x, self.y, plane,
+                alpha=.3,
+                linewidth=0,
+                antialiased=False,
+            )
+        self.ax.set_xlabel('')
+        self.ax.set_zticklabels([])
+        self.ax.set_yticklabels([])
+        self.ax.set_xticklabels([])
+        self.ax.axis([-3., 3., -3., 3.])
+        self.ax.set_zlim([-4., 4.])
 
-# Example 2.
-# Illustrate higher dimensional transformations.
-# Generate the data. 
-n_points = 5
-scale = .1
-# x_green = np.random.normal(loc=-2., scale=scale, size=n_points) 
-# y_green = np.zeros(n_points)
-# z_green = np.zeros(n_points)
-# x_yellow = np.random.normal(loc=-1.4, scale=scale, size=n_points) 
-# y_yellow = np.zeros(n_points)
-# z_yellow = np.zeros(n_points)
-# x_orange = np.random.normal(loc=0., scale=scale, size=n_points) 
-# y_orange = np.zeros(n_points)
-# z_orange = np.zeros(n_points)
-# x_purple = np.random.normal(loc=1.4, scale=scale, size=n_points) 
-# y_purple = np.zeros(n_points)
-# z_purple = np.zeros(n_points)
-# x_black = np.random.normal(loc=2., scale=scale, size=n_points) 
-# y_black = np.zeros(n_points)
-# z_black = np.zeros(n_points)
 
-# Show five types of fruit on a line,
-# based on whether or not it is good to eat.
-fig = plt.figure(7)
-ax = fig.add_subplot(111, projection='3d')
-# x_line = np.arange(-2.1, 2.1, .01)
-# y_line = np.zeros(x_line.size)
-# z_line = np.zeros(x_line.size)
-# ax.plot(x_line, y_line, zs=z_line, color='black')
-# markersize = 40.
-# ax.scatter(
-#     x_green, y_green, zs=z_green,
-#     c='black',
-#     marker='x',
-#     s=markersize,
-# )
-# ax.scatter(
-#     x_yellow, y_yellow, zs=z_yellow,
-#     c='green',
-#     marker='o',
-#     s=markersize,
-# )
-# ax.scatter(
-#     x_orange, y_orange, zs=z_orange,
-#     c='black',
-#     marker='x',
-#     s=markersize,
-# )
-# ax.scatter(
-#     x_purple, y_purple, zs=z_purple,
-#     c='green',
-#     marker='o',
-#     s=markersize,
-# )
-# ax.scatter(
-#     x_black, y_black, zs=z_black,
-#     c='black',
-#     marker='x',
-#     s=markersize,
-# )
+def main():
+    # Hide toolbar from  window
+    rcParams['toolbar'] = 'None'
 
-# ax.set_xlabel('<-- green, yellow, orange, purple, black -->')
-# ax.set_zticklabels([])
-# ax.set_yticklabels([])
-# ax.set_xticklabels([])
-# ax.axis([-3., 3., -3., 3.])
-# ax.set_zlim([-3., 3.])
+    # Initiate class
+    svm = KernelExamples()
+    
+    # Surfaces
+    # svm.quadratic_surface()
+    svm.quadratic_surface2()
+    # svm.rippled_surface()
+    # svm.peaked_surface()
 
-# Transform the data to two dimensions.
-shift = 2.2
-# y_green = x_green**2  - shift
-# y_yellow = x_yellow**2 - shift
-# y_orange = x_orange**2 - shift
-# y_purple = x_purple**2 - shift
-# y_black = x_black**2 - shift
-# fig = plt.figure(8)
-# ax = fig.add_subplot(111, projection='3d')
-# y_line = x_line**2 - shift
-# ax.plot(x_line, y_line, zs=z_line, color='black')
-# ax.scatter(
-#     x_green, y_green, zs=z_green,
-#     c='black',
-#     marker='x',
-#     s=markersize,
-# )
-# ax.scatter(
-#     x_yellow, y_yellow, zs=z_yellow,
-#     c='green',
-#     marker='o',
-#     s=markersize,
-# )
-# ax.scatter(
-#     x_orange, y_orange, zs=z_orange,
-#     c='black',
-#     marker='x',
-#     s=markersize,
-# )
-# ax.scatter(
-#     x_purple, y_purple, zs=z_purple,
-#     c='green',
-#     marker='o',
-#     s=markersize,
-# )
-# ax.scatter(
-#     x_black, y_black, zs=z_black,
-#     c='black',
-#     marker='x',
-#     s=markersize,
-# )
+    # Configure position of graph in canvas
+    plt.subplots_adjust(left=.5, bottom=.2, right=.9, top=.8, wspace=.20, hspace=.20)
 
-# ax.set_xlabel('<-- green, yellow, orange, purple, black -->')
-# ax.set_zticklabels([])
-# ax.set_yticklabels([])
-# ax.set_xticklabels([])
-# ax.axis([-3., 3., -3., 3.])
-# ax.set_zlim([-3., 3.])
+    # showtime
+    plt.show()
 
-# # Transform the data to three dimensions.
-# z_green = y_green**2  - shift
-# z_yellow = y_yellow**2 - shift
-# z_orange = y_orange**2 - shift
-# z_purple = y_purple**2 - shift
-# z_black = y_black**2 - shift
-# fig = plt.figure(9)
-# ax = fig.add_subplot(111, projection='3d')
-# z_line = y_line**2 - shift
-# ax.plot(x_line, y_line, zs=z_line, color='black')
-# ax.scatter(
-#     x_green, y_green, zs=z_green,
-#     c='black',
-#     marker='x',
-#     s=markersize,
-# )
-# ax.scatter(
-#     x_yellow, y_yellow, zs=z_yellow,
-#     c='green',
-#     marker='o',
-#     s=markersize,
-# )
-# ax.scatter(
-#     x_orange, y_orange, zs=z_orange,
-#     c='black',
-#     marker='x',
-#     s=markersize,
-# )
-# ax.scatter(
-#     x_purple, y_purple, zs=z_purple,
-#     c='green',
-#     marker='o',
-#     s=markersize,
-# )
-# ax.scatter(
-#     x_black, y_black, zs=z_black,
-#     c='black',
-#     marker='x',
-#     s=markersize,
-# )
 
-# Optionally, show the resulting quadratic surface.
-show_surface = True
-if show_surface:
-    x = np.arange(-2.5, 2.5, 0.05)
-    y = np.arange(-2.5, 2.5, 0.05)
-    x, y = np.meshgrid(x, y)
-    dual_quad = y**2 - shift
-    ax.plot_surface(
-        x, y, dual_quad,
-        cmap=cm.summer,
-        alpha=.3,
-        linewidth=0,
-        antialiased=False,
-    )
-
-# Optionally, show a good planar discriminator.
-show_discriminator = False
-if show_discriminator:
-    plane = 0. * x
-    ax.plot_surface(
-        x, y, plane,
-        alpha=.3,
-        linewidth=0,
-        antialiased=False,
-    )
-
-ax.set_xlabel('<-- green, yellow, orange, purple, black -->')
-ax.set_zticklabels([])
-ax.set_yticklabels([])
-ax.set_xticklabels([])
-ax.axis([-3., 3., -3., 3.])
-ax.set_zlim([-4., 4.])
-
-plt.show()
+if __name__ == "__main__":
+    main()
